ee1aa5531191f8e873440e6c9ac4c924a625c313
Feitan <55014613+JustFeitan@users.noreply.github.com>
2023-07-04 16:20:07 +0300

    feat(tutorial): complete step 2

--- src/components/TutorialHeader/TutorialHeader.js
+++ src/components/TutorialHeader/TutorialHeader.js
@@ -1,69 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Header,
   HeaderContainer,
-  HeaderName,
-  HeaderNavigation,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
   HeaderMenuButton,
   HeaderMenuItem,
-  HeaderGlobalBar,
-  HeaderGlobalAction,
-  SkipToContent,
+  HeaderName,
+  HeaderNavigation,
+  HeaderSideNavItems,
   SideNav,
   SideNavItems,
-  HeaderSideNavItems,
+  SkipToContent,
 } from '@carbon/react';
-import { Switcher, Notification, UserAvatar } from '@carbon/react/icons';
+import { Notification, Switcher, UserAvatar } from '@carbon/react/icons';
 import { Link } from 'react-router-dom';
 
-const TutorialHeader = () => (
-  <HeaderContainer
-    render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-      <Header aria-label="Carbon Tutorial">
-        <SkipToContent />
-        <HeaderMenuButton
-          aria-label="Open menu"
-          onClick={onClickSideNavExpand}
-          isActive={isSideNavExpanded}
-        />
-        <HeaderName element={Link} to="/" prefix="IBM">
-          Carbon Tutorial
-        </HeaderName>
-        <HeaderNavigation aria-label="Carbon Tutorial">
-          <HeaderMenuItem element={Link} to="/repos">
-            Repositories
-          </HeaderMenuItem>
-        </HeaderNavigation>
-        <SideNav
-          aria-label="Side navigation"
-          expanded={isSideNavExpanded}
-          isPersistent={false}>
-          <SideNavItems>
-            <HeaderSideNavItems>
-              <HeaderMenuItem element={Link} to="/repos">
-                Repositories
-              </HeaderMenuItem>
-            </HeaderSideNavItems>
-          </SideNavItems>
-        </SideNav>
-        <HeaderGlobalBar>
-          <HeaderGlobalAction
-            aria-label="Notifications"
-            tooltipAlignment="center">
-            <Notification size={20} />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction
-            aria-label="User Avatar"
-            tooltipAlignment="center">
-            <UserAvatar size={20} />
-          </HeaderGlobalAction>
-          <HeaderGlobalAction aria-label="App Switcher" tooltipAlignment="end">
-            <Switcher size={20} />
-          </HeaderGlobalAction>
-        </HeaderGlobalBar>
-      </Header>
-    )}
-  />
-);
+const TutorialHeader = () => {
+  const [sideNavState, setSidenavState] = useState(false);
+
+  const toggleSideNav = () => {
+    setSidenavState(prevState => !prevState);
+  };
+
+  return (
+    <HeaderContainer
+      render={() => (
+        <Header aria-label="Carbon Tutorial">
+          <SkipToContent />
+          <HeaderMenuButton
+            aria-label="Open menu"
+            onClick={toggleSideNav}
+            isActive={sideNavState}
+          />
+          <HeaderName element={Link} to="/" prefix="IBM">
+            Carbon Tutorial
+          </HeaderName>
+          <HeaderNavigation aria-label="Carbon Tutorial">
+            <HeaderMenuItem element={Link} to="/repos">
+              Repositories
+            </HeaderMenuItem>
+          </HeaderNavigation>
+          <SideNav
+            aria-label="Side navigation"
+            expanded={sideNavState}
+            isPersistent={false}>
+            <SideNavItems>
+              <HeaderSideNavItems>
+                <HeaderMenuItem
+                  element={Link}
+                  to="/repos"
+                  onClick={toggleSideNav}>
+                  Repositories
+                </HeaderMenuItem>
+              </HeaderSideNavItems>
+            </SideNavItems>
+            {/*Block for close sidebar on overlay click*/}
+            <div
+              className={sideNavState ? `overlay-for-close` : ''}
+              onClick={toggleSideNav}
+            />
+          </SideNav>
+          <HeaderGlobalBar>
+            <HeaderGlobalAction
+              aria-label="Notifications"
+              tooltipAlignment="center">
+              <Notification size={20} />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction
+              aria-label="User Avatar"
+              tooltipAlignment="center">
+              <UserAvatar size={20} />
+            </HeaderGlobalAction>
+            <HeaderGlobalAction
+              aria-label="App Switcher"
+              tooltipAlignment="end">
+              <Switcher size={20} />
+            </HeaderGlobalAction>
+          </HeaderGlobalBar>
+        </Header>
+      )}
+    />
+  );
+};
 
 export default TutorialHeader;
